2e365315ca638bedaa5da330f47741d858c3ff05
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-04-04 20:20:43 -0400

    chore(init): reorder hooks per hook_api.md lifecycle order

--- init.zsh
+++ init.zsh
@@ -1,56 +1,11 @@
 # shellcheck shell=bash
 ######################################################################
-#<
-#
-# Function: p6df::modules::irc::deps()
-#
-#>
-######################################################################
 p6df::modules::irc::deps() {
   ModuleDeps=(
     p6m7g8-dotfiles/p6df-shell
   )
 }
 
-######################################################################
-#<
-#
-# Function: p6df::modules::irc::external::brews()
-#
-#>
-######################################################################
-p6df::modules::irc::external::brews() {
-
-  p6df::core::homebrew::cli::brew::install irssi
-
-  p6_return_void
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::irc::home::symlinks()
-#
-#  Environment:	 P6_DFZ_SRC_P6M7G8_DOTFILES_DIR
-#>
-######################################################################
-p6df::modules::irc::home::symlinks() {
-
-  p6_file_symlink "$P6_DFZ_SRC_P6M7G8_DOTFILES_DIR/p6df-irc/share/irssi" ".irssi"
-
-  p6_return_void
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::irc::aliases::init(_module, dir)
-#
-#  Args:
-#	_module -
-#	dir -
-#
-#>
 ######################################################################
 p6df::modules::irc::aliases::init() {
   local _module="$1"
@@ -64,6 +19,56 @@ p6df::modules::irc::aliases::init() {
 }
 
 ######################################################################
+p6df::modules::irc::home::symlinks() {
+
+  p6_file_symlink "$P6_DFZ_SRC_P6M7G8_DOTFILES_DIR/p6df-irc/share/irssi" ".irssi"
+
+  p6_return_void
+}
+
+######################################################################
+p6df::modules::irc::external::brews() {
+
+  p6df::core::homebrew::cli::brew::install irssi
+
+  p6_return_void
+}
+
+######################################################################
+p6df::modules::irc::profile::mod() {
+
+  p6_return_words 'irc' '$IRCNICK' '$IRCHOST'
+}
+######################################################################
+#<
+#
+# Function: p6df::modules::irc::deps()
+#
+#>
+######################################################################
+#<
+#
+# Function: p6df::modules::irc::external::brews()
+#
+#>
+######################################################################
+#<
+#
+# Function: p6df::modules::irc::home::symlinks()
+#
+#  Environment:	 P6_DFZ_SRC_P6M7G8_DOTFILES_DIR
+#>
+######################################################################
+#<
+#
+# Function: p6df::modules::irc::aliases::init(_module, dir)
+#
+#  Args:
+#	_module -
+#	dir -
+#
+#>
+######################################################################
 #<
 #
 # Function: words irc $IRC_SERVER = p6df::modules::irc::profile::mod()
@@ -73,8 +78,3 @@ p6df::modules::irc::aliases::init() {
 #
 #  Environment:	 IRC_SERVER
 #>
-######################################################################
-p6df::modules::irc::profile::mod() {
-
-  p6_return_words 'irc' '$IRCNICK' '$IRCHOST'
-}
